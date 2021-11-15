d112d29128db93c2a668fbe0b49c6e8c8d7e683a
dphutdt <80830264+dphuytdt@users.noreply.github.com>
2021-11-15 11:59:52 +0700

    fix am pm time

--- public/app.js
+++ public/app.js
@@ -88,11 +88,11 @@ $(document).ready(function () {
   function formatAMPM(date) {
     var hours = date.getHours();
     var minutes = date.getMinutes();
-    var ampm = hours >= 12 ? 'pm' : 'am';
+    var amp = hours >= 12 ? 'pm' : 'am';
     hours = hours % 12;
-    hours = hours ? hours : 12; // the hour '0' should be '12'
+    hours = hours ? hours : 12;
     minutes = minutes < 10 ? '0' + minutes : minutes;
-    var strTime = hours + ':' + minutes + ' ' + ampm;
+    var strTime = hours + ':' + minutes + ' ' + amp;
     return strTime;
   }
 });
